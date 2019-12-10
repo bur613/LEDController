038aee66c12a98282711b795430430047c6e2a54
dom <nikoburns@hotmail.com>
2019-12-10 09:44:10 -0500

    Add gradle

---
+++ app/src/main/java/com/example/batterysensor/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.batterysensor;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView batteryTxt;
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context ctxt, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            batteryTxt.setText(String.valueOf(level) + "%");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        batteryTxt = (TextView) findViewById(R.id.batteryTxt);
+        //batteryTxt.setText("Cool");
+        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        Process process = Runtime.getRuntime().exec("su -c 'echo 0 > /sys/devices/virtual/android_usb/android0/enable'");
+    }
+}
